81c1a239a21daba92c3c1f337a84a223c74df837
glorykas <glorykasongo07@gmail.com>
2020-11-02 15:41:35 +0200

    modify

--- src/sample/io/Api.java
+++ src/sample/io/Api.java
@@ -2,7 +2,7 @@ package sample.io;
 
 public class Api {
    private static String baseURL="http://docker.project45.co.za:8089/p45_api/";
-   //private static String baseURL="http://10.115.3.205:8089/p45_api/";
+   //private static String baseURL="http://192.168.8.107:8089/p45_api/";
    public static String getApi(){
        return baseURL;
    }

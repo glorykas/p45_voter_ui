44d0ec8d5b6e598e5217933542a108676a755cdf
glorykas <glorykasongo07@gmail.com>
2020-11-02 15:54:51 +0200

    modify

--- src/sample/io/Api.java
+++ src/sample/io/Api.java
@@ -1,5 +1,8 @@
 package sample.io;
 
 public class Api {
-
+    private static String baseURL="http://172.20.10.4:8089/p45_api/";
+    public static String getApi(){
+        return baseURL;
+    }
 }

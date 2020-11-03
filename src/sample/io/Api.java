82055405678390960aa7da54281035c0154f154f
glorykas <glorykasongo07@gmail.com>
2020-11-03 11:19:39 +0200

    changes have been made

--- src/sample/io/Api.java
+++ src/sample/io/Api.java
@@ -1,9 +1,6 @@
 package sample.io;
 
 public class Api {
-    private static String baseURL="http://docker.project45.co.za:8089/p45_api/";
-    public static String getApi(){
-        return baseURL;
-    }
+
 
 }
